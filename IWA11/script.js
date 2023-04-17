918a14d4652b8076e3013803ac770166ea9ae4bb
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-04-17 09:58:36 +0200

    IWA11 uncompleted

---
+++ IWA11/script.js
@@ -0,0 +1,56 @@
+// script.js
+
+// const 1-root = document(order1),
+// const 1-biscuits: document(biscuits),
+// const 1-donuts: document(donuts),
+// const 1-pancakes: document(pancakes),
+// const 1-status: document(status)
+
+// const 2-root = document(order2),
+// const 2-biscuits: document(biscuits),
+// const 2-donuts: document(donuts),
+// const 2-pancakes: document(pancakes),
+// const 2-status: document(status)
+
+// const 3-root = document(order3),
+// const 3-biscuits: document(biscuits),
+// const 3-donuts: document(donuts),
+// const 3-pancakes: document(pancakes),
+// const 3-status: document(status)
+
+// 1-biscuits= 1-root.biscuits,
+// 1-donuts = 1-root.donuts,
+// 1-pancakes = 1-root.pancakes,
+// 1-status = 1-root.status ? Delivered : Pending
+
+// 2-biscuits= 2-root.biscuits,
+// 2-donuts = 2-root.donuts,
+// 2-pancakes = 2-root.pancakes,
+// 2-status = 2-root.status ? Delivered : Pending
+
+// 3-biscuits= 3-root.biscuits,
+// 3-donuts = 3-root.donuts,
+// 3-pancakes = 3-root.pancakes,
+//3-status = 3-root.status ? Delivered : Pending
+const first = document.querySelector('[data-biscuits]')
+let info = (first.getAttribute('data-biscuits')) 
+const me = document.querySelector('.count').innerHTML = info
+
+const first1 = document.querySelector('[data-donuts]')
+let info1 = (first.getAttribute('data-donuts'))
+
+
+
+
+// const first1 = document.querySelector('[data-donuts]')
+// let info1 = (first1.getAttribute('data-donuts')) 
+// const me1 = document.querySelector('.count').innerHTML = info1
+// console.log(me1)
+
+
+
+
+
+
+
+
