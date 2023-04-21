d0f6984d5a8e6adee9872f20fc9c3167f85eaf25
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-04-21 16:58:28 +0200

    completed IW14

---
+++ IWA14/challenge1/script.js
@@ -0,0 +1,14 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+  console.log(job())
+  console.log(job())
+}
+
+function job()  {
+  return(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+logTwice()
